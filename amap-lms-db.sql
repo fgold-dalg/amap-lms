6f86e3467929cde3fd851c04c9beb3b40d3eb325
alice <alice.caporali@hotmail.fr>
2024-02-18 19:22:40 +0100

    sql majuscule en minuscule

--- amap-lms-db.sql
+++ amap-lms-db.sql
@@ -1,8 +1,8 @@
 -- Database: AMAP
 
--- DROP DATABASE IF EXISTS "AMAP";
+-- DROP DATABASE IF EXISTS "amap";
 
--- CREATE DATABASE "AMAP"
+-- CREATE DATABASE "amap"
 --    WITH
 --    OWNER = postgres
 --    ENCODING = 'UTF8'
@@ -12,29 +12,29 @@
 --    CONNECTION LIMIT = -1
 --    IS_TEMPLATE = False;
 
--- SCHEMA: LMS
+-- SCHEMA: lms
 
--- DROP SCHEMA IF EXISTS "LMS" ;
+-- DROP SCHEMA IF EXISTS "lms" ;
 
-CREATE SCHEMA IF NOT EXISTS "LMS"
+CREATE SCHEMA IF NOT EXISTS "lms"
     AUTHORIZATION postgres;
 
--- Table: LMS.adherent
+-- Table: lms.adherent
 
--- DROP TABLE IF EXISTS "LMS".adherent;
+-- DROP TABLE IF EXISTS "lms".adherent;
 
-CREATE TABLE IF NOT EXISTS "LMS".adherent
+CREATE TABLE IF NOT EXISTS "lms".adherent
 (
-    "ID_ADH" integer NOT NULL,
-    "NOM" character(30) COLLATE pg_catalog."default",
-    "PRENOM" character(30) COLLATE pg_catalog."default",
-    "ADRESSE" character(200) COLLATE pg_catalog."default",
-    "COURRIEL" character(150) COLLATE pg_catalog."default",
-    "TELEPHONE" numeric(10,0),
-    CONSTRAINT adherent_pkey PRIMARY KEY ("ID_ADH")
+    "id" integer NOT NULL,
+    "nom" character(30) COLLATE pg_catalog."default",
+    "prenom" character(30) COLLATE pg_catalog."default",
+    "adresse" character(200) COLLATE pg_catalog."default",
+    "courriel" character(150) COLLATE pg_catalog."default",
+    "telephone" numeric(10,0),
+    CONSTRAINT adherent_pkey PRIMARY KEY ("id")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS "LMS".adherent
+ALTER TABLE IF EXISTS "lms".adherent
     OWNER to postgres;
