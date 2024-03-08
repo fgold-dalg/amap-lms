e7a395b3849c01426eaad585cdf2716e47418067
fgold-dalg <fgold-dalg@ProjetWeb.akasha>
2024-03-08 22:18:42 +0100

    mise à jour message erreur formulaire ajout adhérent

--- models/form-validator.js
+++ models/form-validator.js
@@ -2,14 +2,14 @@ const { body } = require('express-validator');
 
 // Règles de validation du formulaire d'ajout d'adhérents
 const adherentValidator = [
-  body('nom', 'Erreur, ce champs ne peut être vide').notEmpty(),
-  body('prenom', 'Erreur, ce champs ne peut être vide').notEmpty(),
-  body('adresse', 'Erreur, ce champs ne peut être vide').notEmpty(),
+  body('nom', 'Ce champs ne peut être vide').notEmpty(),
+  body('prenom', 'Ce champs ne peut être vide').notEmpty(),
+  body('adresse', 'Ce champs ne peut être vide').notEmpty(),
   //courriel  
-  body('courriel', 'Erreur, ce champs ne peut être vide').notEmpty(),
-  body('courriel', 'Adresse courriel invalide').isEmail(),
+  body('courriel', 'Ce champs ne peut être vide').notEmpty(),
+  body('courriel', 'Adresse de courriel invalide (nom@mail.fr)').isEmail(),
   //Téléphone
-  body('tel', 'Erreur incorrecte - 10 chiffres attendus').isInt(),
+  body('tel', 'Format incorrect - 10 chiffres attendus').isInt(),
   body('tel', 'Longueur incorrecte - 10 chiffres attendus').isLength({min:10,max:10}),
 ];
 
