5730763956e6bdde31c4c2fc91c6e6f304bab36a
fgold-dalg <fgold-dalg@ProjetWeb.akasha>
2024-03-19 21:32:23 +0100

    correction documentaires et ajout d'une icone au bouton supprimer un adherent

--- models/adherent-models.js
+++ models/adherent-models.js
@@ -4,13 +4,13 @@ var pgp = require("pg-promise")(/*options*/);
 var db = pgp("postgres://"+config.database.user+":"+config.database.pwd+"@"+config.database.host+":"+config.database.port+"/"+config.database.db);
 
 module.exports = {
-    ajout,
+    ajouter,
     recuperer,
-    suppression
+    supprimer
 };
 
 // Requête SQL ajout adhérent
-function ajout(nom,prenom,adresse,courriel,telephone){
+function ajouter(nom,prenom,adresse,courriel,telephone){
     return db.one("INSERT INTO lms.adherent(nom,prenom,adresse,courriel,telephone) VALUES ($1,$2,$3,$4,$5) RETURNING *",[nom,prenom,adresse,courriel,telephone])
 }
 
@@ -21,7 +21,7 @@ function recuperer(nom,prenom,adresse,courriel,telephone){
 } 
 
 // Requête SQL supprimer adhérent
-function suppression(id){
+function supprimer(id){
     // Nativement DELETE ne retourne pas de résultat si la requête est valide. Le "RETURNING" permet d'y remédier.
     return db.one("DELETE FROM lms.adherent WHERE id = $1 RETURNING id",[id]);
 } 
